129061f18f9f9680bb9c7d9438e4b5e650f57630
SergioDeIscarValera <seroigres888@gmail.com>
2022-11-07 19:55:28 +0100

    Funciones Manuales

--- TEMA-02/src/main/kotlin/ExpRegulares/ExpRegularesManuales.kt
+++ TEMA-02/src/main/kotlin/ExpRegulares/ExpRegularesManuales.kt
@@ -1,22 +1,92 @@
 package ExpRegulares
 
 fun main(){
-    val text = "Texto de prueba"
+    var text = "  Texto de prueba  "
+    text = trimManual(text)
     val textSplited = splitManual(text, ' ')
     for (i in textSplited){
         println(i)
     }
+    println(contentToStringManual(textSplited))
+    println(joinToStringManual(textSplited))
+    println(joinToStringManual(reverseManual(textSplited)))
+    println(containsManual(textSplited, "Texto"))
+}
+
+fun containsManual(array: Array<String>, toFind: String): Boolean {
+    for (i in array){
+        if (i.length == toFind.length){
+            var check = true
+            loopPalabra@for (j in i.indices){
+                if (i[j] != toFind[j]){
+                    check = false
+                    break@loopPalabra
+                }
+            }
+            if (check) return check
+        }
+    }
+    return false
+}
+
+fun reverseManual(array: Array<String>): Array<String> {
+    val newArray = Array(array.size){ "" }
+    for (i in array.lastIndex downTo 0){
+        newArray[i] = array[i]
+    }
+    return newArray
+}
+
+fun contentToStringManual(array: Array<String>): String {
+    var arrayContent = "{ "
+    arrayContent += joinToStringManual(array)
+    arrayContent += "}"
+    return arrayContent
+}
+
+fun joinToStringManual(array: Array<String>):String{
+    var arrayContent = ""
+    for (i in array){
+        if (i == array.last())  arrayContent += "$i "
+        else arrayContent += "$i, "
+    }
+    return arrayContent
+}
+
+fun trimManual(text: String): String {
+    var initialIndex = 0
+    var lastIndex = 0
+
+    for (i in text.indices){
+        if (text[i] != ' '){
+            initialIndex = i
+            break
+        }
+    }
+    for (i in text.lastIndex downTo 0){
+        if (text[i] != ' '){
+            lastIndex = i
+            break
+        }
+    }
+
+    var newText = ""
+    for (i in initialIndex..lastIndex){
+        newText += text[i]
+    }
+    return newText
 }
 
 fun splitManual(text: String, delimitador: Char): Array<String>{
-    val array = Array<String>(countChar(text, delimitador)){""}
+    val array = Array<String>(countChar(text, delimitador) + 1){""}
     var lastIndex = 0
     var count = 0
     for (i in text.indices){
-        if (text[i] == delimitador)
+        if (text[i] == delimitador || i == text.lastIndex)
         {
-            for (j in lastIndex until i){
+            for (j in lastIndex.. i){
                 array[count] += text[j].toString()
+                array[count] = trimManual(array[count])
             }
             count++
             lastIndex = i+1
@@ -30,5 +100,5 @@ fun countChar(text: String, char: Char): Int{
     for (i in text){
         if (i == char) count++
     }
-    return count +1
+    return count
 }
